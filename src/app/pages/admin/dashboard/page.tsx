fbdfc44fcda554561b289fd3f5394a3d9c361d4c
Angelo Pane <angipane@hotmail.it>
2025-12-20 02:16:09 +0000

    User password update

--- src/app/pages/admin/dashboard/page.tsx
+++ src/app/pages/admin/dashboard/page.tsx
@@ -20,7 +20,7 @@ const AdminDashboard = () => {
                 <a href="/pages/admin/roles">Role Management</a>
               </li>
               <li>
-                <a href="/pages/admin/password">Change Password</a>
+                <a href="/pages/admin/user">User Password</a>
               </li>
             </ul>
           </div>
